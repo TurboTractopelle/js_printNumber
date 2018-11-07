d61e437a3ea61c300788fa4a7fb809f575ebfb16
TurboTractopelle <44016081+TurboTractopelle@users.noreply.github.com>
2018-11-07 22:09:07 +0100

    add numbers

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
-
 function printNumber(number, char) {
-  
-  let numbers= {
-    1: ["011110", "110011", "110011", "110011", "110011", "011110"]
+  let numbers = {
+    0: ["011110", "110011", "110011", "110011", "110011", "011110"],
+    1: ["001100", "011100", "101100", "001100", "001100", "111111"],
+    2: ["011110", "110011", "000110", "001100", "011000", "111111"],
+    3: ["011110", "110011", "000110", "000110", "110011", "011110"],
+    4: ["110011", "110011", "110011", "011111", "000011", "000011"],
+    5: ["111111", "110000", "111110", "000011", "000011", "111110"]
+  };
+
+  function replaceChar(numberArr) {
+    return numberArr.replace(/0/g, " ").replace(/1/g, char);
   }
 
-  function replaceChar(numberArr){
-    return numberArr.replace(/0/g, ' ').replace(/1/g, char)
-  }
+  let elements = String(number).split("");
 
-  let elements = String(number).split("") // ["1", "1"]
+  let msg = "";
+  let ligne = "";
 
-  let msg="";
-  let ligne="";
-
-  for(let i=0; i < 6; i++){
-    for(let j=0; j<2; j++){
-      ligne += numbers[elements[j]][i] 
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < elements.length; j++) {
+      ligne += numbers[elements[j]][i] + " ";
     }
-    msg += ligne + "\n"
-    ligne = ""   
+    msg += ligne + "\n";
+    ligne = "";
   }
 
-  
-  console.log(replaceChar(msg))
-
+  console.log(replaceChar(msg));
 }
 
-
-
-console.log(printNumber(11,"5"))
-
-
-
+console.log(printNumber(102345, "5"));
